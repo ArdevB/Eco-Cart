89eb8c76c7cbad57438c6b20b22cc73d22a0d8e4
ArdevB <bjrchprnv@gmail.com>
2025-09-21 12:06:33 +0545

    ⚡ feat: enhance POST request handling and improve response structure

--- modules/http.js
+++ modules/http.js
@@ -4,25 +4,27 @@ const app = http.createServer((request, response) => {
   console.log(request.method);
   console.log(request.url);
 
-  if (request.url === "/") {
+  if (request.method == "POST") {
     response.writeHead(200, { "Content-Type": "text/html" });
-    response.end("<h1>Welcome to the E-Commerce API</h1>");
-  } else if (request.url === "/about") {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.end("<h1>About the E-Commerce API</h1>");
+    response.end("<h1>POST request received</h1>");
+    return;
+  } else if (request.method == "GET") {
+    if (request.url === "/") {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.end("<h1>Welcome to the E-Commerce API</h1>");
+    } else if (request.url === "/about") {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.end("<h1>About the E-Commerce API</h1>");
+    } else {
+      response.writeHead(404, { "Content-Type": "text/html" });
+      response.end("<h1>404 Not Found</h1>");
+      return;
+    }
   } else {
-    response.writeHead(404, { "Content-Type": "text/html" });
-    response.end("<h1>404 Not Found</h1>");
+    response.writeHead(405, { "Content-Type": "text/html" });
+    response.end("<h1>Method not allowed</h1>");
     return;
   }
-
-  const data = {
-    version: "0.1.1",
-    name: "E-Commerce API",
-    status: "Running",
-  };
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(data));
 });
 
 app.listen(5000, () => {

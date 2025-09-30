7ba1c2ecbf8b9aa6174fe9171b2c660ffb9dbfa8
ArdevB <bjrchprnv@gmail.com>
2025-09-30 12:39:57 +0545

    ⚡ feat: update authController to use consistent variable naming and improve error handling

--- express-api/src/controllers/authController.js
+++ express-api/src/controllers/authController.js
@@ -19,9 +19,9 @@ const login = async (req, res) => {
 
     const data = await authService.login(input);
 
-    const token = generateJWT(data);
+    const authToken = generateJWT(data);
 
-    const result = await verifyJWT(token);
+    res.cookie("authToken", authToken, { maxAge: 864000 * 1000 });
 
     res.status(200).json(data);
   } catch (error) {
@@ -31,21 +31,24 @@ const login = async (req, res) => {
 
 const register = async (req, res) => {
   const input = req.body;
-
-  if (!input.password) {
-    return res.status(400).send("Password is required.");
-  }
-
-  if (!input.confirmPassword) {
-    return res.status(400).send("Confirm Password is required.");
-  }
-
-  if (input.password !== input.confirmPassword) {
-    return res.status(400).send("Passwords do not match.");
-  }
-
   try {
+    if (!input.password) {
+      return res.status(400).send("Password is required.");
+    }
+
+    if (!input.confirmPassword) {
+      return res.status(400).send("Confirm Password is required.");
+    }
+
+    if (input.password !== input.confirmPassword) {
+      return res.status(400).send("Passwords do not match.");
+    }
+
     const data = await authService.register(req.body);
+
+    const authToken = generateJWT(data);
+
+    res.cookie("authToken", authToken, { maxAge: 864000 * 1000 });
 
     res.status(201).json(data);
   } catch (error) {
